46d2854ba05a36eeb2beb0856178b86d5f148b2e
vincenzo.dev.82.homework <vincenzo.dev.82.homework@gmail.com>
2025-01-06 14:46:31 +0900

    init project versions: spring-boot 3.3.7

---
+++ src/main/kotlin/com/musinsa/ShoppingCategoryApplication.kt
@@ -0,0 +1,11 @@
+package com.musinsa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShoppingCategoryApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShoppingCategoryApplication>(*args)
+}
---
+++ src/test/kotlin/com/musinsa/ShoppingCategoryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.musinsa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShoppingCategoryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
